b15886e41e06653fe4dc093f02c29519a2bc361e
Ana Hord <gordeevaisgreat@gmail.com>
2023-11-26 13:45:30 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const screenWidth = window.screen.width;
 const screenHeight = window.screen.height;
@@ -8,20 +8,9 @@ const screenHeight = window.screen.height;
 function App() {
   const [x, setX] = useState(window.screenX);
   const [y, setY] = useState(window.screenY);
-  const [showLine, setShowLine] = useState(true); // chaneg on mount + delete local storage on mount
-  const [lineY, setLineY] = useState();
-  const id = useRef(Date.now());
+
   useEffect(() => {
     const interval = setInterval(() => {
-      const prev = JSON.parse(localStorage.getItem("y")) || {};
-      prev[id.current] = window.screenY;
-      const prevCopy = { ...prev };
-      delete prevCopy[id.current];
-      if (Object.values(prevCopy).length === 1) {
-        setShowLine(true);
-        setLineY(Object.values(prevCopy)[0] - window.screenY);
-      }
-      localStorage.setItem(`y`, JSON.stringify(prev));
       if (x !== window.screenX || y !== window.screenY) {
         setX(window.screenX);
         setY(window.screenY);
@@ -66,15 +55,6 @@ function App() {
           <li className="ring"></li>
         </ul>
       </div>
-      <div
-        className="line"
-        style={{
-          bottom: lineY + "%",
-          top: (window.screenY / screenHeight) * 150 + "%",
-          left: (window.screenX / screenWidth) * 150 + "%",
-          right: 0,
-        }}
-      />
     </>
   );
 }
